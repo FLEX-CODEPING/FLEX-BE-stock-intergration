9a7f69b41347c0e3c664b812d77942bd350ff7a4
jiwon <kiwijomn@gmail.com>
2024-11-11 14:35:29 +0900

    FLEX-203 ♻️ Refactor: error code class 리팩토링
    <footer>
    #1

--- app/exceptions/error_code.py
+++ app/exceptions/error_code.py
@@ -1,10 +1,19 @@
-from enum import Enum
-from starlette.status import *
+from fastapi import status
 
-class ErrorCode(Enum):
-    KIS_REQUEST_ERROR = (HTTP_500_INTERNAL_SERVER_ERROR, "KIS_REQUEST_ERROR", "KoreaInvestAPI Error")
 
-    def __init__(self, http_status, custom_code, message):
-        self.http_status = http_status
-        self.custom_code = custom_code
-        self.message = message
+class ErrorCode:
+    KIS_REQUEST_FAIL = {
+        "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+        "code": "KIS_001",
+        "message": "한국투자증권 open API 요청에 실패했습니다."
+    }
+    KIS_ACCESS_TOKEN_REQUEST_FAIL = {
+        "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+        "code": "KIS_002",
+        "message": "한국투자증권 접근 토큰 발급 요청에 실패했습니다."
+    }
+    KIS_WEBSOCKET_KEY_REQUEST_FAIL = {
+        "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+        "code": "KIS_003",
+        "message": "한국투자증권 웹소켓 접속 키 발급 요청에 실패했습니다."
+    }
