85eba4e8e9b1d0b668e26c225f93123c826cad54
jiwon <kiwijomn@gmail.com>
2024-11-11 14:34:10 +0900

    FLEX-203 ♻️ Refactor: custom exception 포맷 수정
    <footer>
    #1

--- app/exceptions/custom_exception.py
+++ app/exceptions/custom_exception.py
@@ -1,25 +1,13 @@
-from app.exceptions.error_code import ErrorCode
 from fastapi import HTTPException
-from typing import Optional
 
 
 class BaseCustomException(HTTPException):
-    def __init__(self, error_code: ErrorCode, details: Optional[dict] = None):
-        super().__init__(
-            status_code = error_code.http_status,
-            detail = {
-                "custom_code": error_code.custom_code,
-                "message": error_code.message,
-                "details": details or {}
-            }
-        )
-        self.error_code = error_code
-        self.details = details or {}
+    def __init__(self, error_code: dict, details: dict = None):
+        self.status_code = error_code["status"]
+        self.detail = {
+            "code": error_code["code"],
+            "message": error_code["message"],
+            "details": details or {}
+        }
 
-
-class KoreaInvestException(BaseCustomException):
-    def __init__(self, message: str = None, details: dict = None):
-        error_code = ErrorCode.KIS_ERROR
-        if message:
-            error_code.message = message
-        super().__init__(error_code, details)
+        super().__init__(status_code=self.status_code, detail=self.detail)
