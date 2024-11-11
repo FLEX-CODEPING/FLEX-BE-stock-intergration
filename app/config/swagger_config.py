f87170fb7908a0d7782160742c2b385b33c2fef0
jiwon <kiwijomn@gmail.com>
2024-11-11 09:57:33 +0900

    FLEX-203 ✨ Feat: Swagger configuration 추가
    <footer>
    #1

---
+++ app/config/swagger_config.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+
+
+def custom_openapi(app: FastAPI):
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = get_openapi(
+        title = "FLEX 주식 API",
+        version = "1.0",
+        routes = app.routes,
+    )
+
+    openapi_schema["components"]["securitySchemes"] = {
+        "bearerAuth": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT",
+            "in": "header",
+            "name": "Authorization"
+        }
+    }
+
+    openapi_schema["security"] = [{"bearerAuth": []}]
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+def setup_swagger(app: FastAPI):
+    app.openapi = lambda: custom_openapi(app)
