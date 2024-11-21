e279641f2ec45445d6a195de9e632d804fd25d57
joowojr <joowojr@gmail.com>
2024-11-21 10:17:51 +0900

    FLEX-260 🎨 Style: api path 재정립
    <footer>
    - 관련: #9

--- app/main.py
+++ app/main.py
@@ -43,11 +43,11 @@ env_config = KoreaInvestEnv(config)
 base_headers = env_config.get_base_headers()
 config = env_config.get_full_config()
 
-stock_router = APIRouter(prefix = "/api/kisOpenApi", tags = ["stock"])
+stock_router = APIRouter(prefix = "/api/kis/stocks", tags = ["stock"])
 
 
 @stock_router.get(
-    "/{stock_code}/inquirePrice",
+    "/{stock_code}/inquire-price",
     summary="주식 현재가 시세 API 요청",
     description="Retrieve the latest price information for a specific stock using its stock code."
 )
@@ -58,7 +58,7 @@ async def get_inquire_price(stock_code: str):
 
 
 @stock_router.get(
-    "/daily/tradeVolume",
+    "/daily/trade-volume",
     summary="종목별 일별 매수 & 매도 체결량 API 요청 (종목별일별매수매도체결량 [v1_국내주식-056] - 모의투자 미지원)",
     description="Retrieve the latest price information for a specific stock using its stock code."
 )
@@ -94,7 +94,7 @@ async def get_volume_ranking(
     return korea_invest_client.get_volume_ranking(request)
 
 @stock_router.get(
-    "/daily/itemChartPrice",
+    "/daily/item-chart-price",
     summary="국내 주식 기간별 시세 (일/주/월/년) API 요청 (국내주식기간별시세(일/주/월/년)[v1_국내주식-016])",
     description="Retrieve the latest price information for a specific stock using its stock code."
 )
@@ -115,7 +115,7 @@ korea_invest_websocket = KoreaInvestWebSocketClient(korea_invest_client, websock
 async def startup_event():
     asyncio.create_task(korea_invest_websocket.run())
 
-@app.websocket("/api/stocks/{stock_code}/realTime")
+@app.websocket("/api/stocks/{stock_code}/real-time")
 async def websocket_endpoint(websocket: WebSocket, stock_code: str):
     await websocket.accept()
     try:
