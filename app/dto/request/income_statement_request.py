bd17a817338cdc7f8f35cbf200a4670721f859c1
joowojr <joowojr@gmail.com>
2024-11-25 20:36:36 +0900

    FLEX-281 📝 Docs: swagger docs 작성
    <footer>
    - 관련 : #16

--- app/dto/request/income_statement_request.py
+++ app/dto/request/income_statement_request.py
@@ -1,5 +1,13 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 class IncomeStatementReq(BaseModel):
-    stockCode: str = Field(..., description="필수> 입력 종목코드 (0000: 전체, 기타: 업종코드)", max_length=12)
-    classCode: str = Field(..., description="필수> 분류 구분 코드 (0: 전체, 1: 분기(연 누적))", max_length=2)
+    stockCode: str = Field(...,  description="필수> 입력 종목코드", max_length=12)
+    classCode: str = Field(...,  description="필수> 분류 구분 코드 (0: 전체, 1: 분기(연 누적))", max_length=2)
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "stockCode": "005930",
+                "classCode": "1"
+            }
+        }
+    )
