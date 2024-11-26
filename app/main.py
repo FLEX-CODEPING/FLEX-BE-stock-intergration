d701be6f64a496541941624929ab95198c9b27d0
joowojr <joowojr@gmail.com>
2024-11-26 10:20:12 +0900

    FLEX-283 ♻️ Recycle: 분류별로 API Controller 분리
    <footer>
    - 관련 : #20

--- app/main.py
+++ app/main.py
@@ -49,8 +49,9 @@ env_config = KoreaInvestEnv(config)
 base_headers = env_config.get_base_headers()
 config = env_config.get_full_config()
 
-stock_kis_integration_router = APIRouter(prefix ="/api/kis/stocks", tags = ["stock"])
-
+stock_kis_integration_sheet_router = APIRouter(prefix ="/api/kis/stocks", tags = ["KIS Stock Sheet"])
+stock_kis_integration_ranking_router = APIRouter(prefix ="/api/kis/stocks/ranking", tags = ["KIS Stock Ranking"])
+stock_kis_integration__fluctuation_router = APIRouter(prefix ="/api/kis/stocks", tags = ["KIS Stock Fluctuation"])
 
 @app.get("/health", include_in_schema=False)
 async def health_check():
@@ -58,7 +59,7 @@ async def health_check():
     return {"status": "UP"}
 
 
-@stock_kis_integration_router.post(
+@stock_kis_integration__fluctuation_router.post(
     "/{stock_code}/inquire-price",
     summary="주식 현재가 시세 API 요청",
     description="Retrieve the latest price information for a specific stock using its stock code."
@@ -69,7 +70,7 @@ async def get_inquire_price(stock_code: str):
     return korea_invest_client.get_inquire_price(stock_code)
 
 
-@stock_kis_integration_router.post(
+@stock_kis_integration__fluctuation_router.post(
     "/daily/trade-volume",
     summary="종목별 일별 매수 & 매도 체결량 API 요청 (종목별일별매수매도체결량 [v1_국내주식-056] - 모의투자 미지원)",
     description="Retrieve the latest price information for a specific stock using its stock code."
@@ -81,21 +82,7 @@ async def get_daily_trade_volume(
     korea_invest_client = KoreaInvestRestClient(config, base_headers)
     return korea_invest_client.get_daily_trade_volume(request)
 
-
-@stock_kis_integration_router.post(
-    "/ranking/fluctuation",
-    summary="국내 주식 등락률 순위 API 요청 (국내 주식 등락률 순위[v1_국내주식-088])",
-    description="Retrieve the latest price information for a specific stock using its stock code."
-)
-async def get_ranking_fluctuation(
-    request: RankingFluctuationReq = Body(...)
-):
-    config['is_paper_trading'] = False
-    korea_invest_client = KoreaInvestRestClient(config, base_headers)
-    return korea_invest_client.get_ranking_fluctuation(request)
-
-
-@stock_kis_integration_router.post(
+@stock_kis_integration__fluctuation_router.post(
     "/daily/item-chart-price",
     summary="국내 주식 기간별 시세 (일/주/월/년) API 요청 (국내주식기간별시세(일/주/월/년)[v1_국내주식-016])",
     description="Retrieve the latest price information for a specific stock using its stock code."
@@ -108,8 +95,22 @@ async def get_daily_item_chart_price(
     return korea_invest_client.get_daily_item_chart_price(request)
 
 
-@stock_kis_integration_router.post(
-    "/ranking/volume",
+@stock_kis_integration_ranking_router.post(
+    "/fluctuation",
+    summary="국내 주식 등락률 순위 API 요청 (국내 주식 등락률 순위[v1_국내주식-088])",
+    description="Retrieve the latest price information for a specific stock using its stock code."
+)
+async def get_ranking_fluctuation(
+    request: RankingFluctuationReq = Body(...)
+):
+    config['is_paper_trading'] = False
+    korea_invest_client = KoreaInvestRestClient(config, base_headers)
+    return korea_invest_client.get_ranking_fluctuation(request)
+
+
+
+@stock_kis_integration_ranking_router.post(
+    "/volume",
     summary="국내주식 거래량순위 API 요청 (거래량순위 [v1_국내주식-047])"
 )
 async def get_volume_ranking(
@@ -119,7 +120,7 @@ async def get_volume_ranking(
     korea_invest_client = KoreaInvestRestClient(config, base_headers)
     return korea_invest_client.get_volume_ranking(request)
 
-@stock_kis_integration_router.post(
+@stock_kis_integration_sheet_router.post(
     "/balance-sheet",
     summary="국내주식 대차대조표 API 요청 (국내주식 대차대조표[v1_국내주식-078])"
 )
@@ -130,7 +131,7 @@ async def get_volume_ranking(
     korea_invest_client = KoreaInvestRestClient(config, base_headers)
     return korea_invest_client.get_stock_income_statement(request)
 
-@stock_kis_integration_router.post(
+@stock_kis_integration_sheet_router.post(
     "/income-statement",
     summary="국내주식 손익계산서 API 요청 (국내주식 손익계산서[v1_국내주식-079])"
 )
@@ -164,4 +165,6 @@ async def websocket_endpoint(websocket: WebSocket, stock_code: str):
     except WebSocketDisconnect:
         logger.info(f"Client disconnected for stock {stock_code}")
 
-app.include_router(stock_kis_integration_router)
+app.include_router(stock_kis_integration_sheet_router)
+app.include_router(stock_kis_integration_ranking_router)
+app.include_router(stock_kis_integration__fluctuation_router)
