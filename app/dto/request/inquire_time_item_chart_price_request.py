b0b64e142e36ad76ef4ebbc30743d46c92ee7463
joowojr <joowojr@gmail.com>
2024-12-02 21:40:18 +0900

    FLEX-314 ✨ Feat: 주식당일분봉조회 request dto 구현
    <footer>
    - 관련 #34

--- app/dto/request/inquire_time_item_chart_price_request.py
+++ app/dto/request/inquire_time_item_chart_price_request.py
@@ -1,24 +1,23 @@
 from pydantic import BaseModel, Field, ConfigDict
 
-class InqurieTimeDailyChartPriceReq(BaseModel):
+from pydantic import BaseModel, Field
+from pydantic.config import ConfigDict
+
+class InqurieTimeItemChartPriceReq(BaseModel):
     stockCode: str = Field(
         ...,
         description="종목 코드."
     )  # 종목 코드
-    date: str = Field(
-        ...,
-        description="조회 시작 날짜 (YYYYMMDD 형식). (ex. 20241129)"
-    )
     time: str = Field(
-        description="입력 시간(HHMMSS) (ex. 153000)"
+        "",
+        description='조회 시작일자(HHMMSS) ex) "123000" 입력 시 12시 30분 이전부터 1분 간격으로 조회'
     )
 
     model_config = ConfigDict(
         json_schema_extra={
             "example": {
                 "stockCode": "005930",
-                "date": "20241129",
-                "time": "153000"
+                "time": "123000"
             }
         }
     )
